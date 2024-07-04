eb6fd33e2182e14da46cef39b1e2b8474ce18866
Lars Schöbitz <lschoebitz@ethz.ch>
2024-07-04 16:21:24 +0200

    add events page

---
+++ pages/events/events_data/write_yaml_file.R
@@ -0,0 +1,20 @@
+
+library(yaml)
+library(googlesheets4)
+library(tidyverse)
+
+# source functions --------------------------------------------------------
+
+source(here::here("R/df_to_yaml.R"))
+
+# read data ---------------------------------------------------------------
+
+read_sheet("https://docs.google.com/spreadsheets/d/1VhXDQ1WRkBV9qfMF0w0R9dYKHCWq9To-ml4KGExl204/edit?gid=0#gid=0") |> 
+    write_csv(here::here("pages/events/events_data/tbl-06-fairqual-events.csv"))
+
+events <- read_csv(here::here("pages/events/events_data/tbl-06-fairqual-events.csv")) |> 
+    mutate(date = lubridate::as_date(date))
+
+# functions
+
+df_to_yaml(events, here::here("pages/events/events_data/events.yml"))
